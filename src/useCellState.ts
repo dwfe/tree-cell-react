fbaed9179c2d02f95253d60108aa4aab27bb6ba5
z <alex@dwfe.ru>
2022-10-09 09:08:25 +0300

    .

---
+++ src/useCellState.ts
@@ -0,0 +1,35 @@
+import {Cell, Fn} from '@do-while-for-each/tree-cell'
+import {useCallback, useEffect, useState} from 'react'
+
+export function useCellState<TValue>(val: Fn<TValue> | TValue): [
+  TValue,                     // value
+  (newValue: TValue) => void, // setValue
+  Cell,                       // cell
+] {
+  const [cell] = useState(() => {
+    const cell = new Cell(val);
+    cell.onChange(noop);
+    return cell;
+  });
+  const [value, nextValue] = useState(cell.get());
+  const setValue = useCallback( // to prevent pointless re-renders because 'setValue' can be passed as a prop to components
+    (value: TValue) => cell.set(value),
+    []
+  );
+
+  useEffect(() => {
+    cell.onChange(data => {
+      if (data.error)
+        console.error('state error', data.error);
+      else
+        nextValue(data.value as TValue);
+    });
+    cell.offChange(noop);
+    return () => cell.dispose();
+  }, []);
+
+  return [value, setValue, cell];
+}
+
+function noop() {
+}
